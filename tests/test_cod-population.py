94b2274c69f0a7b5c503c35763a1ab4c7fe352df
Briar <briar.j.mills@gmail.com>
2024-11-01 16:37:04 -0400

    Set up tests

--- tests/test_cod-population.py
+++ tests/test_cod-population.py
@@ -2,6 +2,7 @@ from os.path import join
 
 import pytest
 from hdx.api.configuration import Configuration
+from hdx.data.dataset import Dataset
 from hdx.utilities.downloader import Download
 from hdx.utilities.path import temp_dir
 from hdx.utilities.retriever import Retrieve
@@ -21,6 +22,20 @@ class TestCODPopulation:
         )
         return Configuration.read()
 
+    @pytest.fixture(scope="function")
+    def read_dataset(self, monkeypatch):
+        def read_from_hdx(dataset_name):
+            return Dataset.load_from_json(
+                join(
+                    "tests",
+                    "fixtures",
+                    "input",
+                    f"dataset-{dataset_name}.json",
+                )
+            )
+
+        monkeypatch.setattr(Dataset, "read_from_hdx", staticmethod(read_from_hdx))
+
     @pytest.fixture(scope="class")
     def fixtures_dir(self):
         return join("tests", "fixtures")
@@ -33,7 +48,9 @@ class TestCODPopulation:
     def config_dir(self, fixtures_dir):
         return join("src", "hdx", "scraper", "cod_population", "config")
 
-    def test_cod_population(self, configuration, fixtures_dir, input_dir, config_dir):
+    def test_cod_population(
+        self, configuration, read_dataset, fixtures_dir, input_dir, config_dir
+    ):
         with temp_dir(
             "Testcod-population",
             delete_on_success=True,
@@ -48,11 +65,66 @@ class TestCODPopulation:
                     save=False,
                     use_saved=True,
                 )
-                countries = []
-                cod_population = CODPopulation(
-                    configuration, countries, retriever, tempdir
-                )
-                cod_population.download_country_data()
-                dataset = cod_population.generate_dataset()
+                countries = ["AFG"]
+                cod_population = CODPopulation(configuration, retriever, tempdir)
+                cod_population.download_country_data(countries)
+                assert len(cod_population.data) == 2
+                assert cod_population.data[0][0] == {
+                    "ISO3": "AFG",
+                    "Country": "Afghanistan",
+                    "Population_group": "F_TL",
+                    "Gender": "f",
+                    "Age_range": "all",
+                    "Population": 19844212,
+                    "Date_start": "2021-01-01",
+                    "Date_end": "2021-12-31",
+                    "Source": "National Statistic and Information Authority (NSIA) Afghanistan",
+                }
+                assert cod_population.data[1][0] == {
+                    "ISO3": "AFG",
+                    "Country": "Afghanistan",
+                    "ADM1_PCODE": "AF17",
+                    "ADM1_NAME": "Badakhshan",
+                    "Population_group": "F_TL",
+                    "Gender": "f",
+                    "Age_range": "all",
+                    "Population": 666004,
+                    "Date_start": "2021-01-01",
+                    "Date_end": "2021-12-31",
+                    "Source": "National Statistic and Information Authority (NSIA) Afghanistan",
+                }
+                assert cod_population.metadata == {
+                    "countries": ["AFG"],
+                    "date_end": ["2021-12-31"],
+                    "date_start": ["2021-01-01"],
+                }
 
+                dataset = cod_population.generate_dataset()
                 dataset.update_from_yaml(path=join(config_dir, "hdx_dataset_static.yaml"))
+                assert dataset == {
+                    "name": "cod-ps-global",
+                    "title": "Global Subnational Population Statistics",
+                    "groups": [{"name": "afg"}],
+                    "dataset_date": "[2021-01-01T00:00:00 TO 2021-12-31T23:59:59]",
+                    "tags": [
+                        {
+                            "name": "baseline population",
+                            "vocabulary_id": "b891512e-9516-4bf5-962a-7a289772a2a1",
+                        }
+                    ],
+                    "license_id": "cc-by",
+                    "methodology": "Other",
+                    "methodology_other": "Compiled daily from individual COD population statistics files.",
+                    "caveats": "Please see the files for individual sources, and visit each country page for more detailed methods.",
+                    "dataset_source": "HDX",
+                    "package_creator": "HDX Data Systems Team",
+                    "private": False,
+                    "maintainer": "aa13de36-28c5-47a7-8d0b-6d7c754ba8c8",
+                    "owner_org": "hdx",
+                    "data_update_frequency": 1,
+                    "notes": "Latest COD population statistics compiled at the admin level. The CSV files contain subnational p-codes, their corresponding administrative names, source organization, and reference dates where available. These are constructed from individual country level population files.",
+                    "subnational": 1,
+                }
+
+                resources = dataset.get_resources()
+                assert len(resources) == 2

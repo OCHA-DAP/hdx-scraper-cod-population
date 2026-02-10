be5d9ffe2b407e221d6349df2b4db09c09cbea18
mcarans <rans@email.com>
2026-02-10 15:48:34 +1300

    Update for AdminLevel no hxl

--- src/hdx/scraper/cod_population/cod_population.py
+++ src/hdx/scraper/cod_population/cod_population.py
@@ -362,8 +362,7 @@ class CODPopulation:
     def get_pcodes(self) -> None:
         for admin_level in [1, 2]:
             admin = AdminLevel(admin_level=admin_level, retriever=self._retriever)
-            dataset = admin.get_libhxl_dataset(retriever=self._retriever)
-            admin.setup_from_libhxl_dataset(dataset)
+            admin.setup_from_url()
             admin.load_pcode_formats()
             self._admins.append(admin)
 
